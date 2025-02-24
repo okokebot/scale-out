a8d3cc2d73a905314f46822ff3ce2da42f50c664
okokebot <78920829+okokebot@users.noreply.github.com>
2025-02-25 01:55:59 +0900

    wasm を非同期で初期化しつつ、init の処理を変更. type error も修正 (#4)
    * wasm を非同期で初期化しつつ、init の処理を変更. type error も修正
    
    * build error 対応 (型をimport できておらず)

--- src/_components/Note/NotePresenter.tsx
+++ src/_components/Note/NotePresenter.tsx
@@ -1,5 +1,7 @@
+import { SoundGenerator } from "@/classes/SoundGenerator";
 import { getSoundGenerator } from "@/consts/soundGenerator";
 import { AccidentalType } from "@/types/notes";
+import { useEffect, useState } from "react";
 import { Note } from "../../classes/Notes";
 import "./note.css";
 
@@ -16,17 +18,26 @@ const getAccidentalSymbol = (accidental: AccidentalType | null) => {
 };
 
 export const NotePresenter = ({ note, isRoot }: NotePresenterProps) => {
-  const soundGenerator = getSoundGenerator();
+  const [soundGenerator, setSoundGenerator] = useState<SoundGenerator | null>(
+    null,
+  );
+
+  useEffect(() => {
+    getSoundGenerator().then(setSoundGenerator);
+  }, []);
+
   const letterName = note.letterName;
   const className = isRoot
     ? `note ${letterName.toLowerCase()} root`
     : `note ${letterName.toLowerCase()}`;
   const accidentalClassName = isRoot ? "accidental root" : "accidental";
   const accidental = getAccidentalSymbol(note.accidental);
+
   return (
     <button
       className={className}
-      onClick={() => soundGenerator.playSound(note.frequency)}
+      onClick={() => soundGenerator?.playSound(note.frequency)}
+      disabled={!soundGenerator} // 初期化前はボタンを無効化
     >
       {letterName}
       {note.octave}
